24eeddcad3b7d0a5986f9d714b15b7ff353f09ce
davidechieli <dvdchieli@hotmail.it>
2024-03-18 11:33:51 +0100

    ok

--- src/app/components/logout/logout.component.ts
+++ src/app/components/logout/logout.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-logout',
-  templateUrl: './logout.component.html',
-  styleUrl: './logout.component.scss'
+	selector: "app-logout",
+	templateUrl: "./logout.component.html",
+	styleUrl: "./logout.component.scss",
 })
-export class LogoutComponent {
-
+export class LogoutComponent implements OnInit {
+	constructor(private route: Router) {}
+	ngOnInit(): void {
+		this.route.navigate(["/openstar"]);
+	}
 }
--- src/app/components/my-esg-commitment/my-esg-commitment.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-my-esg-commitment',
-  templateUrl: './my-esg-commitment.component.html',
-  styleUrl: './my-esg-commitment.component.scss',
-})
-export class MyEsgCommitmentComponent {}
