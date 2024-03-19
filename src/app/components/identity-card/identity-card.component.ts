fa114fe3c3b7ae6c4f1f52fc9564798f423a8bd8
davidechieli <dvdchieli@hotmail.it>
2024-03-19 15:34:22 +0100

    added more components

---
+++ src/app/components/identity-card/identity-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-identity-card',
+  templateUrl: './identity-card.component.html',
+  styleUrl: './identity-card.component.scss'
+})
+export class IdentityCardComponent {
+
+}
